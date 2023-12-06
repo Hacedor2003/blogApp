0b25e4b8f0435164bd6ea0bd5aef3bc83862e10d
Hacedor2003 <bryanlenier@gmail.com>
2023-12-06 04:42:51 -0500

    App basica del blog

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -17,7 +25,7 @@ def post_list(request):
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request,'blog/post/list.html',{'page': page,'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
